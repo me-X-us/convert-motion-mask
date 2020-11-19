b2e2999bdcae2b9d6be62d843386babe1e8f4020
LSY <ys_lee1017@naver.com>
2020-11-19 17:38:56 +0900

    feat: instance_segmentation maskrcnn 을 이용한 모델로 변환
    추가사항 :
    기존 shape estimatin 파일 이름 변경

---
+++ instance_segmentation.py
@@ -0,0 +1,111 @@
+import torch
+import torchvision
+from torchvision import models
+import torchvision.transforms as T
+import numpy as np
+import cv2
+import time
+import os
+
+
+"""
+preprocess : Data preprocessing method
+@params
+img : image to process
+"""
+
+
+def preprocess(img):
+    trf = T.Compose([T.ToTensor()])
+    input_img = trf(img).unsqueeze(0)
+    return input_img
+
+
+"""
+seg_map : make segmentation image
+@params
+img : input image
+COLORS : background and human shape colors
+"""
+
+
+def seg_map(img, COLORS):
+    rgb = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    idx = img >= 0.5
+    rgb[idx] = COLORS[1]
+    rgb[idx == False] = COLORS[0]
+    return rgb
+
+
+"""
+shape_estimation
+@params
+filename : Video data's name
+img_size : resoluion, default = 720
+"""
+
+
+def shape_estimation(filename, img_size=720):
+    t = time.time()
+    output_path = os.path.dirname(filename)
+    output_filename = os.path.basename(filename).split(".")[0] + "_shape.mp4"
+    output_filename = os.path.join(output_path, output_filename)
+
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True).eval()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    print("Using.." + device)
+    if device == "cpu":
+        print(f"Can use CPUs -> {torch.get_num_threads()}")
+        torch.set_num_threads(torch.get_num_threads())
+    COLORS = np.array(
+        [(255, 255, 255), (0, 255, 255)]
+    )  # background -> white  # person -> cyan
+    IMG_SIZE = img_size
+
+    # read video data
+    cap = cv2.VideoCapture(filename)
+    assert cap.isOpened()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+    # nof_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    # frame_count = 0
+
+    # video writer (MP4), image shape (height, width, channel)
+    video_format = cv2.VideoWriter_fourcc(*"mp4v")
+    out_video = cv2.VideoWriter(
+        output_filename,
+        video_format,
+        cap.get(cv2.CAP_PROP_FPS),
+        (IMG_SIZE, int(height * IMG_SIZE / width)),
+    )
+    # print(f"Video resolution : {img.shape}")
+
+    while cap.isOpened():
+        ret, img = cap.read()
+        if not ret:
+            break
+        # video resize
+        img = cv2.resize(img, (IMG_SIZE, int(img.shape[0] * IMG_SIZE / img.shape[1])))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        input_tensor = preprocess(img)
+        if device == "cuda":
+            input_tensor = input_tensor.to(device)
+            model = model.to(device)
+        out = model(input_tensor)[0]["masks"][0]  # out.shape (1,height, width)
+        out = out.squeeze().detach().cpu().numpy()
+
+        out = seg_map(out, COLORS)  # color map
+        out = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
+        out_video.write(out)
+        # print("\r Frame : ", int(nof_frame), frame_count)
+        # frame_count = frame_count + 1
+    cap.release()
+    out_video.release()
+    print(f"total time : {(time.time() - t)/60.} min.")  # total time
+
+
+# test code
+# if __name__ == "__main__":
+#     shape_estimation('./testvid.mp4')
